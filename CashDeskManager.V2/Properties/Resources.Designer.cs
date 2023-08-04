f7627cdd7fb29e6efbd495d28d8f01ee947cd5d6
Sait ORHAN <saitorhan@bilisimegitmeni.com>
2023-08-04 10:39:01 +0300

    release

--- CashDeskManager.V2/Properties/Resources.Designer.cs
+++ CashDeskManager.V2/Properties/Resources.Designer.cs
@@ -203,6 +203,26 @@ namespace CashDeskManager.V2.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Google {
+            get {
+                object obj = ResourceManager.GetObject("Google", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pc {
+            get {
+                object obj = ResourceManager.GetObject("pc", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap refresh2_16x16 {
             get {
                 object obj = ResourceManager.GetObject("refresh2_16x16", resourceCulture);
